0e8b5d8a5a1c9abe3b57f041f7058cdffd5afc4e
the-prairie <77875172+the-prairie@users.noreply.github.com>
2021-06-24 22:51:29 -0700

    updated auth

--- plugins/hooks/degreed_hook.py
+++ plugins/hooks/degreed_hook.py
@@ -12,8 +12,8 @@ class DegreedHook(HttpHook):
     Add OAuth support to the basic HttpHook
     """
 
-    def __init__(self, degreed_conn_id, token_url="https://degreed.com/oauth/token", *args, **kwargs) -> None:
-        super().__init__(http_conn_id=degreed_conn_id, *args, **kwargs)
+    def __init__(self, degreed_conn_id, token_url="https://degreed.com/oauth/token") -> None:
+        super().__init__(http_conn_id=degreed_conn_id)
         self.token_url = token_url
  
     # headers may be passed through directly or in the "extra" field in the connection
@@ -27,91 +27,31 @@ class DegreedHook(HttpHook):
 
         client = oauth2.BackendApplicationClient(client_id=conn.login)
         session = requests_oauthlib.OAuth2Session(client=client)
+        token = session.fetch_token(
+                token_url=self.token_url,
+                client_id=conn.login,
+                client_secret=conn.password,
+                scope = 'users:read logins:read pathways:read completions:read views:read required-learning:read',
+                include_client_id=True
+            )
 
-        # inject token to the session
-    
-        session.fetch_token(
-            token_url=self.token_url,
-            client_id=conn.login,
-            client_secret=conn.password,
-            scope = 'users:read logins:read pathways:read completions:read views:read required-learning:read',
-            include_client_id=True
-        )
-        if conn.host and "://" in conn.host:
-            self.base_url = conn.host
-        else:
-            # schema defaults to HTTPS
-            schema = conn.schema if conn.schema else "https"
-            host = conn.host if conn.host else ""
-            self.base_url = schema + "://" + host
+        headers = {'Authorization': 'Bearer {}'.format(token['access_token'])}
+        return headers
 
-        if conn.port:
-            self.base_url = self.base_url + ":" + str(conn.port)
+    def run_request(self,
+            endpoint,
+            payload=None):
+         
+         headers = self.get_conn()
+         response = requests.get(url=endpoint, params=payload , headers=headers)
 
-        if conn.extra:
-            try:
-                session.headers.update(conn.extra_dejson)
-            except TypeError:
-                self.log.warn('Connection to %s has invalid extra field.', conn.host)
-        if headers:
-            session.headers.update(headers)
+         return response.reason
 
-        return session
-    
-    def run(self,
-            endpoint: str,
-            data: OptionalDictAny = None,
-            headers: OptionalDictAny = None,
-            extra_options: OptionalDictAny = None) -> requests.Response:
 
-        session = self.get_conn(headers)
+         
+         
 
-        if self.base_url and not self.base_url.endswith('/') and \
-           endpoint and not endpoint.startswith('/'):
-            url = self.base_url + '/' + endpoint
-        else:
-            url = (self.base_url or '') + (endpoint or '')
 
-        req_settings = dict(
-            stream=extra_options.get("stream", False),
-            verify=extra_options.get("verify", False),
-            proxies=extra_options.get("proxies", {}),
-            cert=extra_options.get("cert"),
-            timeout=extra_options.get("timeout"),
-            allow_redirects=extra_options.get("allow_redirects", True)
-        )
-
-        try:
-            self.log.info("Sending '%s' to url: %s", self.method, url)
-            if self.method == 'GET':
-                # GET uses params
-                response = session.request(self.method,
-                                           url,
-                                           params=data,
-                                           headers=headers,
-                                           **req_settings)
-            elif self.method == 'HEAD':
-                # HEAD doesn't use params
-                response = session.request(self.method,
-                                           url,
-                                           headers=headers,
-                                           **req_settings)
-            else:
-                # Others use data
-                response = session.request(self.method,
-                                           url,
-                                           data=data,
-                                           headers=headers,
-                                           **req_settings)
-
-        except requests.exceptions.ConnectionError as ex:
-            self.log.warning(str(ex) + ' Tenacity will retry to execute the operation')
-            raise ex
-
-        if extra_options.get('check_response', True):
-            self.check_response(response)
-
-        return response
 
 
 
